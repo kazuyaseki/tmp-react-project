700875ccf2e03a21688114868aed537c3d453fae
kazuyaseki <k.seki@kakehashi.life>
2021-06-07 14:24:03 +0900

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button } from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button onClick={() => alert("押された")}>押す</Button>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/hoge">
+          <Link to="">go to top</Link>
+        </Route>
+        <Route path="/">
+          <Link to="hoge">go to hoge</Link>
+          <Button onClick={() => alert("fuga")}>hoge</Button>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
